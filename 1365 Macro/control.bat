20f7074e8360d9086755f2bdb69091f7bb40ef27
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-04-20 18:10:47 +0900

    v1.1.2 Release

--- 1365 Macro/control.bat
+++ 1365 Macro/control.bat
@@ -2,5 +2,14 @@
 setlocal
 cd /d %~dp0
 cd Resources
+
 control.exe
+if not "ERRORLEVEL%" == "0" goto ERROR
+goto OK
+
+:ERROR
+python control.py
+goto OK
+
+:OK
 pause
--- 1365 Macro/tester.bat
+++ 1365 Macro/tester.bat
@@ -2,5 +2,14 @@
 setlocal
 cd /d %~dp0
 cd Resources
+
 tester.exe
+if not "ERRORLEVEL%" == "0" goto ERROR
+goto OK
+
+:ERROR
+python tester.py
+goto OK
+
+:OK
 pause
